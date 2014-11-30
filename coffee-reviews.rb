f38ba6db93811520b0996e5c5dd7fc92bd23a4c8
Allison <allisonmariebrowne@gmail.com>
2014-11-30 06:23:56 -0500

    working on acessing multiple pages of review per coffee maker

--- coffee-reviews.rb
+++ coffee-reviews.rb
@@ -8,29 +8,67 @@ require 'open-uri'
 require 'pry'
 
 
-@coffee_makers_doc = Nokogiri::HTML(open("http://www.amazon.com/Drip-Coffee-Machines-Makers/b?ie=UTF8&node=289745", {"User-Agent"=>"Mozilla"}))
-@urls = @coffee_makers_doc.css("span.asinReviewsSummary a").map { |link| link['href'] }
+# @coffee_makers_doc = Nokogiri::HTML(open("http://www.amazon.com/Drip-Coffee-Machines-Makers/b?ie=UTF8&node=289745", {"User-Agent"=>"Mozilla"}))
+# @urls = @coffee_makers_doc.css("span.asinReviewsSummary a").map { |link| link['href'] }
+#
+# @reviews = Array.new
+# @ratings = Array.new
 
-@reviews = Array.new
-@ratings = Array.new
 
+# @urls.each do |url|
+#   @coffee_maker_doc = Nokogiri::HTML(open(url, {"User-Agent"=>"Mozilla"}))
+#   @ratings_noko = @coffee_maker_doc.css('table#productReviews span.swSprite.s_star')
+#   i=0
+#   while i<= (@ratings_noko).length do
+#     @ratings << (@coffee_maker_doc.css('table#productReviews span.swSprite')[i].text).chr
+#     @reviews << @coffee_maker_doc.css("div.reviewText")[i].text
+#     i+=1
+#   end
+#
+#   @ratings_and_reviews = Hash[@ratings.zip(@reviews)]
+# end
 
-@urls.each do |url|
-  @coffee_maker_doc = Nokogiri::HTML(open(url, {"User-Agent"=>"Mozilla"}))
-  @product_title = @coffee_maker_doc.css('span#productTitle').text
-  @ratings_noko = @coffee_maker_doc.css('table#productReviews span.swSprite.s_star')
-  i=0
-  while i<= (@ratings_noko).length do
-    @ratings << (@coffee_maker_doc.css('table#productReviews span.swSprite')[i].text).chr
+url_one_page_ratings='http://www.amazon.com/Keurig-Elite-Brewing-System-Black/product-reviews/B00AQ9NIO0/ref=lp_289745_1_1_cm_cr_acr_txt?ie=UTF8&showViewpoints=1'
 
-    @reviews << @coffee_maker_doc.css("div.reviewText")[i].text
-    i+=1
+def get_noko_data(url)
+  Nokogiri::HTML(open(url, {"User-Agent"=>"Mozilla"}))
+end
+
+doc_one_page = get_noko_data(url_one_page_ratings)
+
+def get_ratings(doc)
+  ratings = Array.new
+  doc.css('table#productReviews span.swSprite:first-child').each do |rating|
+    ratings << rating.text.chr
   end
-  binding.pry
-  @ratings_and_reviews = Hash[@ratings.zip(@reviews)]
+  ratings.reject!.with_index{|_, i| i.odd?}
+end
+
+def get_reviews(doc)
+  reviews = Array.new
+  doc.css("div.reviewText").each do |review|
+    reviews << review.text
+  end
+  reviews
+end
+
+#for ratings go through each page and each time concatinate old array to new.
+def get_next_page_url(first_page_doc)
+  first_page_doc.css('span.paging:first-of-type a:last-of-type').first
 end
 
 
+(puts get_next_page_url(doc_one_page))['href'] 
+
+#go through each page and each time concatinate old array to new
+
+#@ratings_and_reviews = Hash[(@ratings.zip(@reviews)]
+
+
+
+# def  get_rating(doc)
+#   doc.css('table#productReviews span.swSprite.s_star')
+# end
 
 
 
